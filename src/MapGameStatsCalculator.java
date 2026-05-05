a17f54d98b5a7bed3f98c5410d662e84b597e8d9
HENRY <henry.garcia1120@icloud.com>
2026-05-04 19:39:24 -0700

    get average score implemented, pretty simple method !

--- src/MapGameStatsCalculator.java
+++ src/MapGameStatsCalculator.java
@@ -20,6 +20,7 @@ public class MapGameStatsCalculator implements GameStatsCalculator {
    */
   private Map<String, Integer> gameCounts;
   private Map<String, Integer> highScores;
+  private Map<String, Integer> totalScores;
 
   // For some waves you will need to add more private instance variables here!
 
@@ -49,6 +50,14 @@ public class MapGameStatsCalculator implements GameStatsCalculator {
     } else {
       highScores.put(name, score);
     }
+
+    if(totalScores.containsKey(name)) 
+      {
+        totalScores.put(name, totalScores.get(name) + score);
+      } else 
+        {
+        totalScores.put(name, score);
+      }
 }
   }
 
@@ -148,10 +157,17 @@ public class MapGameStatsCalculator implements GameStatsCalculator {
   @Override
   public double getAverageScore(String person) {
     // TODO: remove this exception once you have implemented your method!
-    throw new UnsupportedOperationException("Unimplemented method 'getAverageScore'");
+    //throw new UnsupportedOperationException("Unimplemented method 'getAverageScore'");
+
+    checkPerson(person);
+
+    int total = totalScores.get(person);
+    int count = gameCounts.get(person);
+
+    return (double) total / count;
 
     // Uncomment this and have it as your first line once you remove the UnsupportedOperationException
-    //checkPerson(person);
+    
   }
 
   /**
